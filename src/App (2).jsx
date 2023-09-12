4444f7fb13111cb59c0fb0c01a8c477fd4bdf610
Aniruddha Mukherjee <aniruddhaindia1993@gmail.com>
2023-09-12 18:52:18 +0530

    first commit

---
+++ src/App (2).jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Product from './Product'
+import './global.css'
+
+const App = () => {
+  return (
+    <div>
+    
+    <Product/>
+    </div>
+
+  )
+}
+
+export default App
